d12a4007b120c0afd7911c3efc0ed3e5799128e2
matthew-matvei <matthew.d.james87@gmail.com>
2023-06-18 10:40:12 +1000

    Give demo more meaningful title

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -11,13 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Physical Boundaries',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: MyHomePage(
-        title: 'Flutter Demo Home Page',
+        title: 'Physical Boundaries Sample',
         counterRepository: _counterRepository,
       ),
     );
